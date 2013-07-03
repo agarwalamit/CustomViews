db463f862929bee42304f7aecb5c2593f9d49cb1
agarwalamit <amit.agarwal@flipkart.com>
2013-07-03 15:49:06 +0530

    widgets discussed

--- src/com/example/customviews/CustomHorizontalScrollView.java
+++ src/com/example/customviews/CustomHorizontalScrollView.java
@@ -1,10 +1,25 @@
 package com.example.customviews;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.util.AttributeSet;
-import android.view.Gravity;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
@@ -14,13 +29,28 @@ import android.widget.TextView;
 
 public class CustomHorizontalScrollView extends HorizontalScrollView{
 
+	//screen padding
+	private final int SCREEN_LEFT_PADDING_DP = 5;
+	private final int SCREEN_TOP_PADDING_DP = 10;
+	private final int SCREEN_RIGHT_PADDING_DP = 5;
+	private final int SCREEN_BOTTOM_PADDING_DP = 10;
+
+	private final int IMAGE_LEFT_PADDING_DP = 5;
+	private final int IMAGE_TOP_PADDING_DP = 5;
+	private final int IMAGE_RIGHT_PADDING_DP = 5;
+	private final int IMAGE_BOTTOM_PADDING_DP = 10;
+
+	private final int IMAGE_LEFT_MARGIN_DP = 3;
+	private final int IMAGE_RIGHT_MARGIN_DP = 3;
+
+	private final int TEXT_VIEW_MARGIN_TOP_DP = 5;
+	private final int FONT_SIZE_SP = 12;
+
 	LinearLayout ll;
 	Context context;
 
 	public CustomHorizontalScrollView(Context context) {
-		this(context, null);	
-		this.context = context;
-		
+		this(context, null);			
 	}
 
 
@@ -28,82 +58,213 @@ public class CustomHorizontalScrollView extends HorizontalScrollView{
 	public CustomHorizontalScrollView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		this.context= context;
-		ll = new LinearLayout(context);
-		ll.setOrientation(LinearLayout.HORIZONTAL);
-
+		//params for the CustomHorizontalScrollView
 		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
 				LinearLayout.LayoutParams.MATCH_PARENT,
 				LinearLayout.LayoutParams.WRAP_CONTENT);
-
-		
-		ll.setPadding(0,dpToPx(10),dpToPx(10), dpToPx(10));
-		addView(ll);
-		
 		setLayoutParams(lp);
 		setBackgroundResource(R.drawable.storefrontbackground);
+
+		//the linearlayout for scrollview
+		ll = new LinearLayout(context);
+		ll.setOrientation(LinearLayout.HORIZONTAL);
+		ll.setPadding(dpToPx(SCREEN_LEFT_PADDING_DP),dpToPx(SCREEN_TOP_PADDING_DP),dpToPx(SCREEN_RIGHT_PADDING_DP), 
+				dpToPx(SCREEN_BOTTOM_PADDING_DP));		
+
+		addView(ll);
 	}
 
 	//adding images
-	public void addStoreFrontImage(int imageId,String line1Text,String line2Text){
+	//	public void addStoreFrontImage(String imageUrl,String line1Text,String line2Text){
+	//
+	//		StoreFrontImage storeFrontImage = new StoreFrontImage(context, null);
+	//		storeFrontImage.addStoreFrontImage(imageUrl,line1Text,line2Text);		
+	//		storeFrontImage.setOnClickListener(new OnClickListener() {
+	//
+	//			@Override
+	//			public void onClick(View arg0) {
+	//			}
+	//		});
+	//		ll.addView(storeFrontImage);		
+	//	}
 
 
-		StoreFrontImage storeFrontImage = new StoreFrontImage(context, null);
-		storeFrontImage.addStoreFrontImage(imageId,line1Text,line2Text);		
-		storeFrontImage.setOnClickListener(new OnClickListener() {
+	public void addStoreFrontImage(String json,String line1Text,String line2Text){
 
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				System.out.println("asdjfajs");
+		ArrayList<URL> imageUrlList = new ArrayList<URL>();
+		getImageUrlFromJson(json,imageUrlList);
+		for(int i=0;i<imageUrlList.size();i++){
+			StoreFrontImage storeFrontImage = new StoreFrontImage(context, null);
+			storeFrontImage.addStoreFrontImage(imageUrlList.get(i),line1Text,line2Text);		
+			storeFrontImage.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View arg0) {
+				}
+			});
+			ll.addView(storeFrontImage);	
+		}
+	}
+	public void getImageUrlFromJson(String json, ArrayList<URL> imageUrlList ){
+		try{
+			JSONObject jsonObject = new JSONObject(json);
+			JSONArray jsonArray = jsonObject.getJSONArray("items");
+			for(int i=0;i<jsonArray.length();i++){				
+				JSONObject obj = jsonArray.getJSONObject(i);				
+				JSONObject value = obj.getJSONObject("value");				
+				JSONObject image = value.getJSONObject("image");
+
+				//here are the screen sizes 240, etc 
+				DisplayMetrics metrics = this.getResources().getDisplayMetrics();
+				int width = metrics.widthPixels;
+				String screenWidth = ""+width;
+				String urlString = image.getString(screenWidth);		
+				try{
+					URL url=new URL(urlString);
+					imageUrlList.add(url);
+					System.out.println("this "+url);
+				}
+				catch (MalformedURLException e) {
+					System.out.println("error malformed url");
+				}
 
 			}
-		});
-		ll.addView(storeFrontImage);		
-	}
-	private int dpToPx(int dp)
-	{
-	    float density = context.getApplicationContext().getResources().getDisplayMetrics().density;
-	    return Math.round((float)dp * density);
+		}catch (JSONException e) {
+			System.out.println("json exception" + e);
+		}
+
+
+
+
+
+
+
 	}
 
 	private class StoreFrontImage extends LinearLayout {
+		Bitmap bitMap;
+		ImageView iv;
+		URL imageUrl;
 
 		public StoreFrontImage(Context context, AttributeSet attrs) {
 			super(context, attrs);
-			setOrientation(LinearLayout.VERTICAL);
+
+			DisplayMetrics metrics = this.getResources().getDisplayMetrics();
+			int screenWidth = metrics.widthPixels;
+			int totalPaddingDp = SCREEN_LEFT_PADDING_DP + SCREEN_RIGHT_PADDING_DP + 3*(IMAGE_LEFT_MARGIN_DP + IMAGE_RIGHT_MARGIN_DP);
 			LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-					0,
-					LayoutParams.WRAP_CONTENT,1f);			
-			
-			setPadding(dpToPx(5),dpToPx(5),dpToPx(5),dpToPx(10) );
-			//setPadding(5, 5, 5, 10);
-			lp.setMargins(10,0, 0,0);
+					(screenWidth-dpToPx(totalPaddingDp))/3,
+					LayoutParams.WRAP_CONTENT);	
+			lp.setMargins(dpToPx(IMAGE_LEFT_MARGIN_DP),0,dpToPx(IMAGE_RIGHT_MARGIN_DP),0);
+
 			setLayoutParams(lp);
+			setOrientation(LinearLayout.VERTICAL);
 			setBackgroundColor(Color.WHITE);
+			setPadding(dpToPx(IMAGE_LEFT_PADDING_DP),dpToPx(IMAGE_TOP_PADDING_DP),dpToPx(IMAGE_RIGHT_PADDING_DP),
+					dpToPx(IMAGE_BOTTOM_PADDING_DP));
+
 
 		}
-		private int dpToPx(int dp)
-		{
-		    float density = context.getApplicationContext().getResources().getDisplayMetrics().density;
-		    return Math.round((float)dp * density);
-		}
-		public void addStoreFrontImage(int imageId,String line1Text,String line2Text){
+
+		public void addStoreFrontImage(URL imageUrl,String line1Text,String line2Text){
 			LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams
-					(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-			ImageView iv=new ImageView(context);
-			iv.setScaleType(ScaleType.CENTER);
-			iv.setImageResource(imageId);
+					(dpToPx(95), dpToPx(117));
+			iv=new ImageView(context);
+			iv.setBackgroundColor(0x00000000);
+			iv.setScaleType(ScaleType.FIT_CENTER);
+			this.imageUrl = imageUrl;
+			setBitMapFromUrl(imageUrl);
 			addView(iv, imageParams);			
 			
+			//textview
+			LinearLayout.LayoutParams textViewParams = new LinearLayout.LayoutParams
+					(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+			textViewParams.setMargins(0, dpToPx(TEXT_VIEW_MARGIN_TOP_DP), 0, 0);
 			TextView line1=new TextView(context);
 			line1.setText(line1Text);
 			line1.setTypeface(Typeface.DEFAULT_BOLD);
+			line1.setLayoutParams(textViewParams);		
+			line1.setTextSize(TypedValue.COMPLEX_UNIT_SP,FONT_SIZE_SP);
 			addView(line1);
 
+			LinearLayout.LayoutParams textViewParams2 = new LinearLayout.LayoutParams
+					(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 			TextView line2=new TextView(context);
 			line2.setText(line2Text);
 			line2.setTypeface(Typeface.DEFAULT_BOLD);
+			line2.setLayoutParams(textViewParams2);
+			line2.setTextSize(TypedValue.COMPLEX_UNIT_SP,FONT_SIZE_SP);
 			addView(line2);
-		}	
+		}
+		public void setBitMapFromUrl(URL url){
+			MyTask myTask = new MyTask();
+			myTask.execute();
+		}
+		private class MyTask extends AsyncTask<Void, Void, Void>{
+			@Override
+			protected void onPreExecute() {
+				super.onPreExecute();
+			}
+
+			@Override
+			protected Void doInBackground(Void... params) {
+				try{
+					HttpURLConnection conn = (HttpURLConnection) imageUrl.openConnection();   
+					conn.setDoInput(true);   
+					conn.connect();     
+					InputStream is = conn.getInputStream();
+					bitMap = BitmapFactory.decodeStream(is); 						
+				}
+				catch (IOException e) {
+					System.out.println("ioexception occured");
+				}
+				return null;
+			}
+			@Override       
+			protected void onPostExecute(Void args) {
+				iv.setImageBitmap(bitMap);				
+			}
+		}
+		//to be deleted
+		public void addStoreFrontImage(String imageId,String line1Text,String line2Text){
+			/*
+		LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams
+				(dpToPx(95), dpToPx(117));
+		iv=new ImageView(context);
+		iv.setBackgroundColor(0x00000000);
+		iv.setScaleType(ScaleType.FIT_CENTER);
+		try{
+			url = new URL(imageId);
+		}
+		catch (MalformedURLException e) {
+			System.out.println("malformed url occured");
+		}
+		setBitMapFromUrl(imageId);
+		addView(iv, imageParams);			
+		LinearLayout.LayoutParams textViewParams = new LinearLayout.LayoutParams
+				(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		textViewParams.setMargins(0, dpToPx(TEXT_VIEW_MARGIN_TOP_DP), 0, 0);
+		TextView line1=new TextView(context);
+		line1.setText(line1Text);
+		line1.setTypeface(Typeface.DEFAULT_BOLD);
+		line1.setLayoutParams(textViewParams);		
+		line1.setTextSize(TypedValue.COMPLEX_UNIT_SP,FONT_SIZE_SP);
+		addView(line1);
+
+		LinearLayout.LayoutParams textViewParams2 = new LinearLayout.LayoutParams
+				(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		TextView line2=new TextView(context);
+		line2.setText(line2Text);
+		line2.setTypeface(Typeface.DEFAULT_BOLD);
+		line2.setLayoutParams(textViewParams2);
+		line2.setTextSize(TypedValue.COMPLEX_UNIT_SP,FONT_SIZE_SP);
+		addView(line2);*/
+	}
+
+	}
+	private int dpToPx(int dp)
+	{
+		float density = context.getApplicationContext().getResources().getDisplayMetrics().density;
+		return Math.round((float)dp * density);
 	}
 }
--- src/com/example/customviews/MainActivity.java
+++ src/com/example/customviews/MainActivity.java
@@ -1,23 +1,72 @@
 package com.example.customviews;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
 public class MainActivity extends Activity{
 
 	com.example.customviews.CustomHorizontalScrollView hView;
+
+	String url1,url2,url3;
+
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		hView = (com.example.customviews.CustomHorizontalScrollView)findViewById(R.id.hscrollview);
-		
+
 		//getImageResourceFromJson();
-		hView.addStoreFrontImage(R.drawable.lifestyle_fashion,"LIFESTYLE", "&FASHION");
-		hView.addStoreFrontImage(R.drawable.electronics_gadgets,"ELECTRO", "&GADGETS");		
-		hView.addStoreFrontImage(R.drawable.books_media,"BOOKS", "&MEDIA");
+		AssetManager am = this.getAssets();
+		InputStream is = null;
+		try {
+			is = am.open("Json");
+		} catch (IOException e) {
+			System.out.println("problem reading file");
+			e.printStackTrace();
+		}
+		String json = getJsonFromFile(is);
+		url1="http://img6a.flixcart.com/image/t-shirt/s/n/g/wsmad-kb-t1-tee09turquoise-madagascar-400x400-imadkfvvwvmzvcjz.jpeg";
+		url2="http://img6a.flixcart.com/image/t-shirt/s/n/g/wsmad-kb-t1-tee09turquoise-madagascar-400x400-imadkfvvwvmzvcjz.jpeg";
+		url3="http://img6a.flixcart.com/image/t-shirt/s/n/g/wsmad-kb-t1-tee09turquoise-madagascar-400x400-imadkfvvwvmzvcjz.jpeg";
+		//		hView.addStoreFrontImage(url1,"LIFESTYLE", "&FASHION");
+		//		hView.addStoreFrontImage(url2,"ELECTRONICS", "&GADGETS");		
+		//		hView.addStoreFrontImage(url3,"BOOKS", "&MEDIA");
+		
+		hView.addStoreFrontImage(json, "line1Text","line2Text");
+	}
+	public String getJsonFromFile(InputStream is){
+		String json="";
+		BufferedReader br = null;
+
+		try {
+
+			String sCurrentLine;
+
+			br = new BufferedReader(new InputStreamReader(is));
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				json = json + sCurrentLine;
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("here ioexception");
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return json;
 	}
 
-	
 
 
 }
